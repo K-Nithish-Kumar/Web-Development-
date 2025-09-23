44dffa80171e9bf97b81abb5754df28b1c62d263
Nithish kumar k <nithish81812@gmail.com>
2025-09-23 18:33:31 +0530

    Adding JS mini practice project 1

---
+++ JavaScript/JS_Project_01/index.js
@@ -0,0 +1,10 @@
+const name = document.getElementById("Name-change");
+const btn = document.getElementById("Name-Change-btn");
+
+btn.addEventListener("click", function() {
+    document.getElementById("Name-change").innerHTML = "Jeyaram S";
+})
+
+// function nameChange() {
+    
+// }
