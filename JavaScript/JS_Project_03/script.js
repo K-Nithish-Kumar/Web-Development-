87ef9821655cba3dfdd68629bc654f87e743aca4
Nithish kumar k <nithish81812@gmail.com>
2025-09-24 20:17:35 +0530

    Adding childElementCount uses

---
+++ JavaScript/JS_Project_03/script.js
@@ -0,0 +1,4 @@
+const sidebar = document.getElementById('sidebar');
+
+// The childElementCount read-only property returns the number of child elements of this element.
+alert(sidebar.childElementCount)
