5508089320e89d33bd7e7fe24b3110bbe14c8088
Nithish kumar k <nithish81812@gmail.com>
2025-09-25 23:05:52 +0530

    Adding children property

--- JavaScript/JS_Project_03/script.js
+++ JavaScript/JS_Project_03/script.js
@@ -1,4 +1,9 @@
 const sidebar = document.getElementById('sidebar');
 
 // The childElementCount read-only property returns the number of child elements of this element.
-alert(sidebar.childElementCount)
+alert(sidebar.childElementCount);
+
+// children property is used to access the children inside an  element
+for(const child of sidebar.children){
+    alert(child.tagName);
+}
