75f6abccf57ec4d0c1cbf48cdab9464b5d9526ea
K-Nithish-Kumar <nithish81812@gmail.com>
2025-11-01 16:49:46 +0530

    Adding changes in javascript

---
+++ JS-Foundation/Part-1/changes.js
@@ -0,0 +1,12 @@
+let gameName = "PUBG";
+
+gameName = "Vice City";
+
+console.log(gameName);
+
+
+const userName = "kumar";
+
+//userName = "Nithish"; // we cannot overwrite with const variable
+
+console.log(userName);
