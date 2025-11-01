b940be1f7ff9930fca7b426bb46450fe9142acc7
K-Nithish-Kumar <nithish81812@gmail.com>
2025-11-01 16:50:43 +0530

    Adding assignment operator of javascript

---
+++ JS-Foundation/Part-1/assignment.js
@@ -0,0 +1,18 @@
+let score = 10 + 11; // assigning a value
+
+
+let num1 = 10;
+
+// num1 = num1 + 5;
+
+//Shorthand property
+num1 += 5;
+/*
+num1 -= 5;
+num1 *= 5;
+num1 /= 5;
+num1 %= 5;
+*/
+console.log(num1);
+
+
