49131c8da8c2aded818abff8a2f8ffe9301cb050
K-Nithish-Kumar <nithish81812@gmail.com>
2025-11-01 16:46:40 +0530

    Adding datatypes of javascript

---
+++ JS-Foundation/Part-1/datatype.js
@@ -0,0 +1,34 @@
+console.log("Hello");
+// console.log("Nithish");
+
+/*
+
+String - used quite a lot
+Number - used quite a lot
+Boolean - used quite a lot
+BigInt - less used
+
+Undefined - used quite a lot
+null - empty
+
+Object
+
+Symbol - used for make anything unique
+
+*/
+
+
+// var score = 102; // var - old keyword
+
+let score = 102; //Number Type
+let name = "Nithish Kumar"; //String Type
+let isLoggedIN = false; //Boolean
+
+
+//Object
+let cake = ["Black Forest","White Forest","Chocolate"];
+let user = {firstname: "Nithish" ,lastname: "kumar"}
+
+let getScore = score; // borrowing another variable value
+
+console.log(getScore);
