55214632e7e71aa7685d595b3214dd50fb775f40
K-Nithish-Kumar <nithish81812@gmail.com>
2025-11-01 16:45:27 +0530

    Adding printing methods of javascript

---
+++ JS-Foundation/Part-1/printing.js
@@ -0,0 +1,11 @@
+console.log("Hello, Nithish!");
+console.log("Hello, Nithish!");
+
+//The below printing statement does'nt printing the second one on the next line but console had a built in newline
+process.stdout.write("Hello,Nithish!");
+process.stdout.write("Hello,Nithish!");
+
+console.table({City: "Thoothukudi"})
+console.warn({City: "Thoothukudi"})
+
+console.error('Hello ji');
