d8f52fb13d0310f67da7e401c569169d6be155e5
K-Nithish-Kumar <nithish81812@gmail.com>
2025-11-01 16:48:06 +0530

    Adding operations of javascript

---
+++ JS-Foundation/Part-1/operations.js
@@ -0,0 +1,30 @@
+let score = 102;
+let bonus = 25;
+
+let total = score + bonus;
+
+let addition = 4 + 5;
+let subtract = 9 - 3;
+let mult = 3 * 5;
+let divi = 10 / 2;
+let remainder = 10 % 2;
+let expo = 2 ** 3; // 2 power 3
+
+let myScore = 110;
+myScore++;
+
+let credits = 50;
+credits--;
+
+// Comparision Operation
+
+let num1 = 10;
+let num2 = 10;
+let num3 = 15;
+
+console.log(num1 == num2); //gives boolean value - true
+console.log(num1 != num3); // true
+console.log(num1 > num3); // false
+console.log(num1 < num3); // true
+
+
