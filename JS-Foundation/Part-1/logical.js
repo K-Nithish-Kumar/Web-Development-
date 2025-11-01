3523419a5313e37a982f24a9231177d7bb405bb9
K-Nithish-Kumar <nithish81812@gmail.com>
2025-11-01 16:48:59 +0530

    Adding logical operators of javascript

---
+++ JS-Foundation/Part-1/logical.js
@@ -0,0 +1,15 @@
+// && -> and
+// || -> or
+// ! -> reverse
+
+
+let isLoggedIn = true;
+let isPaid = false;
+
+console.log(isLoggedIn && isPaid); // If i want to check both the conditions are we use &&  - false because isPaid is false
+
+
+let isEmailUser = true;
+let isGoogleUSer = false;
+
+console.log(isEmailUser || isGoogleUSer); // If you want either one condition satisfy true use || - true
