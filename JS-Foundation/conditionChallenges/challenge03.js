5c8f126cf3e3c942a8cf1dd86c54be9dbcbfa133
K-Nithish-Kumar <nithish81812@gmail.com>
2025-12-01 13:04:57 +0530

    Adding challenge03 of condition for checking the given variable is equals to the type number or not

---
+++ JS-Foundation/conditionChallenges/challenge03.js
@@ -0,0 +1,10 @@
+// checking if a variable name is a number or not 
+
+
+let balance = 22;
+
+if (typeof(balance) == 'number') {
+    console.log("Yep, this is a number");
+} else {
+    console.log("Nope,it's not a number");
+}
