e3a6329bbbe0b550451d261ef2147764d94a3e5b
K-Nithish-Kumar <nithish81812@gmail.com>
2025-12-01 13:11:37 +0530

    Adding challenge 5 for checking condition of an array is empty or not

---
+++ JS-Foundation/conditionChallenges/challenge05.js
@@ -0,0 +1,9 @@
+// checking if an array is empty or not
+
+let arr = [];
+
+if (arr.length === 0) {
+    console.log("An Array is empty")
+} else {
+    console.log(`Array contains some value ${arr}`);
+}
