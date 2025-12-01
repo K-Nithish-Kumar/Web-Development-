2e554a3a02b15e368c299e3254bec0da7aedbd6c
K-Nithish-Kumar <nithish81812@gmail.com>
2025-12-01 13:24:13 +0530

    Adding challeng 02 of an array for accessing the 3rd element from an array using square bracket and store in another variable

---
+++ JS-Foundation/ArrayChallenges/challenge02.js
@@ -0,0 +1,9 @@
+/*
+Declare an array named "Cities" containing "London", "Tokyo","Paris" and "New York".
+Access the 3rd element in the array and store it in a variable named "FavouriteCity".
+*/
+
+let Cities = ["London","Tokyo","Paris","New York"];
+
+let FavouriteCity = Cities[2];
+console.log(FavouriteCity);
