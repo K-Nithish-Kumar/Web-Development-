f9bb0fb0e53c56918b37c3f26d478281e00e6a24
K-Nithish-Kumar <nithish81812@gmail.com>
2025-12-01 14:04:48 +0530

    Adding the challenge 10 of an array for finding the element are in an array and storing the result in another variable

---
+++ JS-Foundation/ArrayChallenges/challenge10.js
@@ -0,0 +1,8 @@
+/*
+You have an array named "cityBucketList" containing "kyoto","London","Cape Town" and "Vancouver"
+check if "London" is in the array and store the result in a variable named "isLondonInList"
+*/
+
+let cityBucketList = ["kyoto","London","Cape Town","Vancouver"]
+let isLondonInList = cityBucketList.includes("London");
+console.log(isLondonInList);
