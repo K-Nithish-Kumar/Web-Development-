532a5d479e6877e4cecfecc295917c12d67f0687
K-Nithish-Kumar <nithish81812@gmail.com>
2025-12-01 13:59:52 +0530

    Adding challenge 09 of an array for checking the length of an array and storing it into another variable

---
+++ JS-Foundation/ArrayChallenges/challenge09.js
@@ -0,0 +1,8 @@
+/* 
+you have an array named "teaMenu" contaning "masala chai","oolong tea", "green tea" and "earl grey"
+find the length of the array and store it in a var named "menu length"
+*/
+
+let teaMenu = ["masala chai","oolong tea", "green tea","earl grey"];
+let menuLength = teaMenu.length;
+console.log(menuLength);
