3101d57bdc7bd963b902ebf7ce48efe8efdf148a
K-Nithish-Kumar <nithish81812@gmail.com>
2025-12-01 13:29:13 +0530

    Adding challenge 3 of an array for changing the second element of an array

---
+++ JS-Foundation/ArrayChallenges/challenge03.js
@@ -0,0 +1,8 @@
+/*
+You have an array named "teaType" containing "Herbal tea", "White tea" and "Masala chai"
+change the second element of the array to "Jasmine tea"
+*/
+
+let teaType = ["Herbal tea","White tea","Masala chai"];
+teaType[1] = "Jasmine tea"
+console.log(teaType);
