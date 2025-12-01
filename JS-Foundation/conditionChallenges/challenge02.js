c4812cd1bc347382ebb4319c2c63a31e61123896
K-Nithish-Kumar <nithish81812@gmail.com>
2025-12-01 13:01:24 +0530

    Adding challenge 2 of condition checking for same user name

---
+++ JS-Foundation/conditionChallenges/challenge02.js
@@ -0,0 +1,11 @@
+// checking if a string is equal to another string
+
+let userName = "Nithish";
+let anotherUserName = "Nithish";
+
+
+if (userName == anotherUserName) {
+    console.log("Pick another user name for your account")
+} else {
+    console.log("Good name")
+}
