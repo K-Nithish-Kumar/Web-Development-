da4619917e9cb180ecb462227b951ed27f56e2e2
K-Nithish-Kumar <nithish81812@gmail.com>
2025-12-01 13:18:21 +0530

    Adding first challenge of the array of accessing the 1st element of an array and store it an another array

---
+++ JS-Foundation/ArrayChallenges/challenge01.js
@@ -0,0 +1,8 @@
+/*Declare an array named "teaFlavors" that contains the strings "green tea","black tea",and "oolong tea"
+Access the 1st element of the array and store it in a var named firstTea
+*/
+
+let teaFlavors = ["green tea", "black tea", "oolong tea"];
+let firstTea = teaFlavors[0];
+console.log(firstTea);
+
