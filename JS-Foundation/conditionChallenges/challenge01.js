f74da35d636bdd64070a28efffe250322c973901
K-Nithish-Kumar <nithish81812@gmail.com>
2025-12-01 12:57:25 +0530

    Adding challenge 1 of javascript

---
+++ JS-Foundation/conditionChallenges/challenge01.js
@@ -0,0 +1,10 @@
+// checking if a number is greater than another number 
+
+let num1 = 5;
+let num2 = 8;
+
+if(num1 > num2) {
+    console.log("Number 1 is greater");    
+} else {
+    console.log("Nope, Number 2 is greater");
+}
