1be165798a6d2966816eb09594e80ed65af288f0
K-Nithish-Kumar <nithish81812@gmail.com>
2025-12-01 13:38:06 +0530

    Adding challenge 05 of array for removing the last element from an array and storing it to the another variable

---
+++ JS-Foundation/ArrayChallenges/challenge05.js
@@ -0,0 +1,11 @@
+/* 
+You have an array named "teaOrder" with "chai","iced tea","matcha" and "earl grey"
+
+Remove the last element of the array using the pop method and store it in a variable named "lastOrder"
+*/
+
+let teaOrder = ["chai","iced tea","matcha","earl grey"];
+let lastOrder = teaOrder.pop();
+
+console.log(lastOrder);
+
