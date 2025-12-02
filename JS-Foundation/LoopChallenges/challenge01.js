2871db0bd481d6d9c0a6684bf11bb8fe437fb644
K-Nithish-Kumar <nithish81812@gmail.com>
2025-12-02 13:23:59 +0530

    Adding challenge 1 of while loop for sum of all numbers from 1 to 5

---
+++ JS-Foundation/LoopChallenges/challenge01.js
@@ -0,0 +1,13 @@
+/* 
+Write a while loop that calculates the sum of all numbers from 1 to 5 and store the result in a variable named sum
+*/
+
+let i = 1;
+let sum = 0;
+
+while (i <= 5) {
+    sum = sum + i;
+    i++;
+}
+
+console.log(sum);
