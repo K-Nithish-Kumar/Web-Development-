3a143ee0d37abf9186e736fa4cc4dbd90c780033
K-Nithish-Kumar <nithish81812@gmail.com>
2025-12-05 14:29:33 +0530

    Adding challenge07 for function

---
+++ JS-Foundation/FunctionChallenges/challenge07.js
@@ -0,0 +1,11 @@
+/* 
+Write a function flipBoolean that takes any input and converts it to
+its boolean equivalent, then flips it For eg, true become false, 0 become true, etc
+*/
+
+function flipBoolean(input) {
+    return !input;
+}
+
+console.log(flipBoolean(""));
+console.log(flipBoolean("123"));
