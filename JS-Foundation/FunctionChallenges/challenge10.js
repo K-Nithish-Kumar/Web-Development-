4091d5ac70feb554d06dcd2d1cfd6a978635b190
K-Nithish-Kumar <nithish81812@gmail.com>
2025-12-06 18:56:38 +0530

    Adding 11th challnge of function for reversing the array

---
+++ JS-Foundation/FunctionChallenges/challenge10.js
@@ -0,0 +1,11 @@
+/*
+Write a function reverseArray(arr) that reverse the array
+*/
+
+let arr = [7,6,5,4,3,2,1,0];
+
+function reverseArray(arr) {
+    return arr.reverse();
+}
+
+console.log(reverseArray(arr));
