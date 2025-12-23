b1520ce4bcfcdbdeb9b8e393bdf51462e199d139
K-Nithish-Kumar <nithish81812@gmail.com>
2025-12-23 22:36:52 +0530

    Adding constructor function

--- JS-OOPs/constructorFunction.js
+++ JS-OOPs/constructorFunction.js
@@ -38,3 +38,25 @@ Animal.prototype.sound = function () {
 
 let Dog = new Animal("Dog");
 console.log(Dog.sound());
+
+
+// create a tshirt product constructor which have brand,price,color,discount
+
+function Tshirt (brand,price,color,discount) {
+    this.brand = brand;
+    this.price = price;
+    this.color = color;
+    this.discount = discount;
+
+    this.detail = function () {
+        return ` This ${this.color} color Tshirt in ${this.brand} with the price of ${this.price} available with the discount of ${this.discount}`;
+    };
+};
+
+let AllenSollyTshirt = new Tshirt("Allen Solly","Rs 800","Black","20%")
+
+console.log(AllenSollyTshirt);
+console.log(AllenSollyTshirt.detail());
+
+
+
