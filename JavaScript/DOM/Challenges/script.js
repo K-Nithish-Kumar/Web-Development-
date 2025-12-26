59321e61f6da2d47b843f7f001bda2b76a515feb
K-Nithish-Kumar <nithish81812@gmail.com>
2025-12-26 13:10:39 +0530

    Adding 5 challenges of DOM

--- JavaScript/DOM/Challenges/script.js
+++ JavaScript/DOM/Challenges/script.js
@@ -5,4 +5,41 @@ const changeTextButton = document.getElementById('changeTextButton');
 
 changeTextButton.addEventListener('click',function () {
     myParagraph.textContent = 'The Paragraph is changed';
+});
+
+// Example - 2
+
+const citiesList = document.getElementById('citiesList');
+const highlightBtn = document.getElementById('highlightFirstCity');
+
+highlightBtn.addEventListener('click',function () {
+    citiesList.firstElementChild.classList.add('hightlight');
+});
+
+// Example - 3
+const coffeeType = document.getElementById('coffeeType');
+const changeOrder =  document.getElementById('changeOrder');
+
+changeOrder.addEventListener('click', function () {
+    coffeeType.textContent = 'Espresso';
+});
+
+// Example - 4 
+const addNewItem = document.getElementById('addNewItem');
+const shoppingList = document.getElementById('shoppingList');
+
+addNewItem.addEventListener('click', function () {
+    let newItem = document.createElement('li');
+    newItem.textContent = 'Egg'
+
+    shoppingList.lastElementChild.appendChild(newItem);
+});
+
+// Example - 5
+
+const taskList = document.getElementById('taskList');
+const removeLastTask = document.getElementById('removeLastTask');
+
+removeLastTask.addEventListener('click', function () {
+    taskList.lastElementChild.remove();
 })
