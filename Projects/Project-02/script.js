801e1c04da5cf8d7ca42cb32f8b71596d6aa33d8
K-Nithish-Kumar <nithish81812@gmail.com>
2026-01-23 19:51:33 +0530

    Adding animated login page

---
+++ Projects/Project-02/script.js
@@ -0,0 +1,13 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click' , () => {
+    container.classList.add('right-panel-active');
+    document.getElementsByClassName("overlay-left").style.background="linear-gradient(to right,#4f3479, #8f24c7)";
+});
+
+signInButton.addEventListener('click', () => {
+    container.classList.remove('right-panel-active');
+    document.getElementsByClassName("overlay-right").style.background="linear-gradient(to right,#8f24c7, #4f3479)";
+});
